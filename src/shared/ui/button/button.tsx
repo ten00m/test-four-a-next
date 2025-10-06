93c97a9db0c2e067399f6aa0e97ffbd5fb1ece9c
ten00m <ilya.safronov.1989@gmail.com>
2025-10-06 04:01:33 +0300

    style changes to button

--- src/shared/ui/button/button.tsx
+++ src/shared/ui/button/button.tsx
@@ -13,13 +13,12 @@ export const Button: FC<IButtonProps> = ({
     ...props
 }) => {
     const baseClasses =
-        'px-4 py-2 rounded-md font-medium transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed active:scale-95'
+        'px-4 py-2 text-[#191E1F] font-bold rounded-[1.125rem] transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed'
 
     const variantClasses = {
-        primary:
-            'bg-blue-600 text-white hover:bg-blue-700 focus:ring-blue-500',
+        primary: 'bg-primary hover:bg-primary focus:ring-primary',
         secondary:
-            'bg-gray-600 text-white hover:bg-gray-700 focus:ring-gray-500',
+            'bg-gray-600 hover:bg-gray-700 focus:ring-gray-500',
     }
 
     const buttonClasses = `${baseClasses} ${variantClasses[variant]} ${className}`
